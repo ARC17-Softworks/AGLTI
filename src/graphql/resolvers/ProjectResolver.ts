d4b0e847aa72196cbf013e2772385b04e78da939
Bilal-Z <bilalzubairi@hotmail.com>
2020-12-21 00:48:02 +0500

    ProjectResolver: add getProject query

--- src/graphql/resolvers/ProjectResolver.ts
+++ src/graphql/resolvers/ProjectResolver.ts
@@ -1,10 +1,4 @@
-import {
-	// Arg,
-	Ctx,
-	Resolver,
-	Query,
-	UseMiddleware,
-} from 'type-graphql';
+import { Arg, Ctx, Resolver, Query, UseMiddleware } from 'type-graphql';
 import { ProjectResponse } from '../types/ResponseTypes';
 import { authorize, protect } from '../../middleware/auth';
 import { ProjectModel } from '../../entities/Project';
@@ -40,6 +34,24 @@ export class ProjectResolver {
 
 		if (!project) {
 			throw new ApolloError('project does not exist');
+		}
+
+		return { project };
+	}
+
+	@Query(() => ProjectResponse)
+	@UseMiddleware(protect, authorize('BOTH'))
+	async getProject(
+		@Arg('projectId') projectId: string
+	): Promise<ProjectResponse> {
+		const project = await ProjectModel.findById(projectId)
+			.select('-posts -openings -applicants -offered -tasks')
+			.populate('owner', 'name avatar')
+			.populate('members.dev', 'name avatar')
+			.populate('previousMembers.dev', 'name avatar');
+
+		if (!project) {
+			throw new ApolloError(`Resource not found with id of ${projectId}`);
 		}
 
 		return { project };
