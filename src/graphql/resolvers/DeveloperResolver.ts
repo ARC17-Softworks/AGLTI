4856554bd56e7d87462f7b692200226a0a1f2e4b
Bilal-Z <bilalzubairi@hotmail.com>
2020-12-28 23:47:45 +0500

    DeveloperResolver: add leaveProject mutation

--- src/graphql/resolvers/DeveloperResolver.ts
+++ src/graphql/resolvers/DeveloperResolver.ts
@@ -5,6 +5,7 @@ import { authorize, protect } from '../../middleware/auth';
 import { MyContext } from '../types/MyContext';
 import { Types } from 'mongoose';
 import { DocumentType } from '@typegoose/typegoose';
+import { ProfileModel } from '../../entities/Profile';
 
 @Resolver()
 export class DeveloperResolver {
@@ -39,6 +40,58 @@ export class DeveloperResolver {
 			project!.tasks![taskIndex].note = undefined;
 		}
 
+		await project!.save();
+		return true;
+	}
+
+	@Mutation(() => Boolean)
+	@UseMiddleware(protect, authorize('MEMBER'))
+	async leaveProject(@Ctx() ctx: MyContext): Promise<Boolean> {
+		const project = await ProjectModel.findById(ctx.req.project);
+		const profile = await ProfileModel.findOne({ user: ctx.req.user!.id });
+
+		const developer = project!.members!.find(
+			(member) => member.dev!.toString() === ctx.req.user!.id.toString()
+		);
+
+		if (!developer) {
+			throw new ApolloError('user not part of project');
+		}
+
+		// if developer did not contribute to project
+		if (
+			!project!.tasks!.some(
+				(task) =>
+					task.dev!.toString() === ctx.req.user!.id.toString() &&
+					task.status === 'COMPLETE'
+			)
+		) {
+			// remove project from profile
+			profile!.projects!.shift();
+		} else {
+			// add to past members
+			project!.previousMembers!.push(developer);
+		}
+
+		// remove from members
+		project!.members = project!.members!.filter(
+			(member) => member != developer
+		);
+		// unset active project
+		profile!.activeProject = undefined;
+		// clear project forun mentions
+		profile!.mentions = [];
+
+		// remove tasks of dev
+		project!.tasks = project!.tasks!.filter(
+			(task) =>
+				!(
+					task.dev!.toString() === ctx.req.user!.id.toString() &&
+					task.status != 'COMPLETE'
+				)
+		);
+
+		await profile!.save();
 		await project!.save();
 		return true;
 	}
