b6c25fa6d6bb74c92e19746b2fa8fca8eadfdc53
Bilal-Z <bilalzubairi@hotmail.com>
2021-03-18 05:34:28 +0500

    ProjectResolver: add rateColleague mutation

--- seeder.ts
+++ seeder.ts
@@ -12,6 +12,7 @@ import { ProfileModel as Profile } from './src/entities/Profile';
 import { ProjectModel as Project } from './src/entities/Project';
 import { PostionModel as Position } from './src/entities/Position';
 import { MessageThreadModel as MessageThread } from './src/entities/MessageThread';
+import { ProfileReviewModel as ProfileReview } from './src/entities/ProfileReview';
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -30,6 +31,10 @@ const profiles = JSON.parse(
 	fs.readFileSync(`${__dirname}/_data/profiles_v3.json`, 'utf-8')
 );
 
+const profilereviews = JSON.parse(
+	fs.readFileSync(`${__dirname}/_data/profilereviews.json`, 'utf-8')
+);
+
 const projects = JSON.parse(
 	fs.readFileSync(`${__dirname}/_data/projects_v3.json`, 'utf-8')
 );
@@ -45,6 +50,7 @@ const importData = async () => {
 		await Profile.create(profiles);
 		await Project.create(projects);
 		await Position.create(positions);
+		await ProfileReview.create(profilereviews);
 		console.log('Data Imported...'.green.inverse);
 		process.exit();
 	} catch (err) {
@@ -60,6 +66,7 @@ const deleteData = async () => {
 		await Project.deleteMany({});
 		await Position.deleteMany({});
 		await MessageThread.deleteMany({});
+		await ProfileReview.deleteMany({});
 		console.log('Data Destroyed...'.red.inverse);
 		process.exit();
 	} catch (err) {
