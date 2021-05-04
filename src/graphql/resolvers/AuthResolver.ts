649d42431ff55b4fe4e3701a1627e17e25c7c25c
Bilal <bilalzubairi@hotmail.com>
2021-05-04 10:44:41 +0500

    Update cookie options

--- src/graphql/resolvers/AuthResolver.ts
+++ src/graphql/resolvers/AuthResolver.ts
@@ -283,6 +283,8 @@ const setTokenCookie = (user: DocumentType<User>, res: Response) => {
 			Date.now() + Number(process.env.JWT_COOKIE_EXPIRE) * 24 * 60 * 60 * 1000
 		),
 		httpOnly: true,
+		sameSite: 'none',
+		secure: true,
 	};
 
 	if (process.env.NODE_ENV === 'production') {
